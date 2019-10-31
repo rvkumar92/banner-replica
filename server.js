2ca99d4ff044d2709ff13734d79af7a0d5dc234e
Vijay Kumar Ravikumar <vijaykumar.r@listertechnologies.com>
2019-10-31 20:39:33 +0530

    banner replication practice using uikit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("lib"));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8000, () => console.log("Server listening on port 8000"));
